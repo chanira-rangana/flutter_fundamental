0fe2d5c4e46ef35ba489a5e980cdd38bad134dcc
Chanira <chanirapannala@gmail.com>
2024-03-24 11:00:08 +0530

    feat: LinearGradient and text styling

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,18 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.purpleAccent,
-      body: Container(child: const Center(child: Text("Hello World"))),
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color.fromRGBO(255, 26, 2, 80), Colors.white],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const Center(
+              child: Text(
+            "Hello World",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ))),
     ),
   ));
 }
