37749db19961e77fec902f8ac3a493d657352212
Chanira <chanirapannala@gmail.com>
2024-03-24 23:06:43 +0530

    feat: classes anf objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.purpleAccent,
-      body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Color.fromRGBO(255, 26, 2, 80), Colors.white],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-          ),
-          child: const Center(
-              child: Text(
-            "Hello World",
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          ))),
-    ),
+        backgroundColor: Colors.purpleAccent, body: GradientContiner()),
   ));
+}
+
+class GradientContiner extends StatelessWidget {
+  const GradientContiner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Color.fromRGBO(255, 26, 2, 80), Colors.white],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+        ),
+        child: const Center(
+            child: Text(
+          "Hello World",
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        )));
+  }
 }
